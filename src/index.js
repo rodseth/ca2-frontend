56fbf54f0b08ec0d5aa9fb38da360262f1f3197d
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-03-29 15:44:38 +0200

    made a lot of the groundwork

--- src/index.js
+++ src/index.js
@@ -29,6 +29,13 @@ function makeTableRow(name){
   const finishedtable = tableheaders+joinedResult;
   return finishedtable;
 }
+function makeSingleTableRow(name){
+  const list = `<tr> <td> ${name.email}</td> <td>${name.firstName}</td> <td>${name.lastName}</td></tr>`
+  const tableheaders =  "<tr><th>email</th><th>firstName</th><th>lastName</th></tr>"
+  const finishedtable = tableheaders+list;
+  return finishedtable;
+}
+
 
 function getPeople(){
   fetch("http://localhost:8080/ca2/api/persons")
@@ -37,22 +44,74 @@ function getPeople(){
             document.getElementById("allPersons").innerHTML=makeTableRow(data);
         })
 }
-function getPersonByID(){
-  fetch("http://localhost:8080/ca2/api/persons/id/1")
+function displayAmount(count){
+  return count.count;
+}
+function getTotalAmountOfPeople(){
+  fetch("http://localhost:8080/ca2/api/persons/count")
         .then(res=>res.json())
         .then(data=> {
-            document.getElementById("allPersons").innerHTML=makeTableRow(data);
+            document.getElementById("amountOfUsers").innerText=displayAmount(data);
         })
+}
+
+function getAmountOfPeopleWithGivenHobby(){
+  let hobby = document.getElementById("amountOfPeopleWithGivenHobby").value;
+  fetch("http://localhost:8080/ca2/api/persons/count/"+hobby)
+  .then(res=>res.json())
+  .then(data=> {
+      document.getElementById("amountOfPeopleWithGivenHobbyResult").innerText=displayAmount(data);
+  })
+}
+function getPersonByID(){
+  let id = document.getElementById("personID").value;
+  fetch("http://localhost:8080/ca2/api/persons/id/"+id)
+        .then(res=>res.json())
+        .then(data=> {
+            document.getElementById("personByID").innerHTML=makeSingleTableRow(data);
+        })
+}
+
+function deletePerson(event){
+  let id = document.getElementById("deletePersonID").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("DELETE", "http://localhost:8080/ca2/api/persons/delete/"+id, true);
+  xhttp.send();
+
+ document.getElementById("wasDeletedOrNot").innerHTML="Deleted person with id: " + id;
+ 
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "postnumre": hideAllShowOne("postnumre_html"); break
-    case "hobbies": hideAllShowOne("hobbies_html"); break
-    case "personer": hideAllShowOne("personer_html"); getPeople() ; break
-    case "administrer": hideAllShowOne("administrer_html"); break
-    default: hideAllShowOne("home_html"); break
+    case "postnumre": hideAllShowOne("postnumre_html"); 
+    break
+    
+    case "hobbies": hideAllShowOne("hobbies_html");  
+    break
+
+    case "personer": hideAllShowOne("personer_html"); 
+    
+    getPeople() ; 
+    
+    getTotalAmountOfPeople();
+    
+    document.getElementById("searchHobby").onclick = () =>getAmountOfPeopleWithGivenHobby();
+    
+    document.getElementById("search").onclick = () => getPersonByID();
+   
+   
+    break
+    
+    case "administrer": hideAllShowOne("administrer_html"); 
+
+    document.getElementById("deletePerson").onclick = () => deletePerson(onclick) 
+
+    break
+
+    default: hideAllShowOne("home_html");
+    break
   }
   evt.preventDefault();
 }
@@ -66,6 +125,8 @@ function menuItemClicked(evt) {
 // }
 
 document.getElementById("menu").onclick = menuItemClicked;
+
+
 // document.getElementById("search").addEventListener("click", hideAllShowOne("search_html"));
 hideAllShowOne("home_html");
 
